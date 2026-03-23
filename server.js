d915fb80e369f626048f18a23408663f08baddad
arbimalik <arbi.malik@gmail.com>
2026-03-22 21:16:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ const data = await response.json();
 res.status(response.ok ? 200 : 400).json(data);
 } catch (err) { res.status(500).json({error: err.message}); }
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() { console.log('OK port ' + PORT); });
